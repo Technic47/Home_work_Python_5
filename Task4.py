f4ab1c110c4e8133ca30090e116599b11ee45afd
Pavel <Technic47@gmail.com>
2022-09-18 14:53:14 +0300

    RLE try

--- Task4.py
+++ Task4.py
@@ -2,18 +2,21 @@
 from random import randint
 
 
-def generate_text(size: int = 10000) -> None:
+def generate_text(path: str, size: int = 10000) -> None:
+    """Generate random string with size = size and saves it in file Generated_text.txt"""
     text = ''.join(str(randint(0, 1)) for i in range(size))
-    with open(r'Generated_text.txt', 'w') as data:
+    with open(path, 'w') as data:
         data.write(text)
 
 
 def copy_text(path: str) -> str:
+    """Copies file from path"""
     with open(path, 'r') as data:
         text = data.read()
     return text
 
-def write_text(text) -> None:
+def write_text(path, text) -> None:
+    """Write text to file"""
     with open(r'Generated_text.txt', 'a') as data:
         data.writelines(f'\n{text}')
 
@@ -38,13 +41,11 @@ def zipp(text: str) -> str:
     # data_str = str(data)
     return data_str
 
-
-generate_text(100)
 path = r'Generated_text.txt'
+generate_text(path, 100)
 text = copy_text(path)
 print(text)
 data = zipp(text)
 print(data)
 print(len(text), len(data))
-
-write_text(data)
+write_text(path, data)
