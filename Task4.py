5145d7f7eb7a3e566457bd708461b5468029e48c
Pavel <Technic47@gmail.com>
2022-09-18 17:05:11 +0300

    RLE done

--- Task4.py
+++ Task4.py
@@ -1,9 +1,10 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 from random import randint
+from itertools import groupby
 
 
 def generate_text(path: str, size: int = 10000) -> None:
-    """Generate random string with size = size and saves it in file Generated_text.txt"""
+    """Generates random string with size = size and saves it in file Generated_text.txt"""
     text = ''.join(str(randint(0, 1)) for i in range(size))
     with open(path, 'w') as data:
         data.write(text)
@@ -15,14 +16,15 @@ def copy_text(path: str) -> str:
         text = data.read()
     return text
 
+
 def write_text(path, text) -> None:
-    """Write text to file"""
+    """Writes text to file"""
     with open(r'Generated_text.txt', 'a') as data:
         data.writelines(f'\n{text}')
 
 
 def zipp(text: str) -> str:
-    # data = []
+    """Encodes with LRE"""
     data_str = ''
     i = 0
     while i < len(text):
@@ -33,19 +35,45 @@ def zipp(text: str) -> str:
             j += 1
             if j == len(text):
                 break
-        # block = [size, int(text[i])]
-        # data.append(block)
         data_str += str(size)
         data_str += text[i]
+
         i += size
-    # data_str = str(data)
     return data_str
 
+
+def unzipp(text: str) -> str:
+    """Decodes LRE text"""
+    data_str = ""
+    i = 0
+    while i <= len(text) - 1:
+        count = int(text[i])
+        word = text[i + 1]
+        for j in range(count):
+            data_str += word
+            j += 1
+        i += 2
+    return data_str
+
+
+def zipp_groupby(text: str) -> str:
+    """Encodes with LRE using groupby"""
+    return "".join(f"{sum(1 for _ in x)}{y}" for y, x in groupby(text))
+
+
 path = r'Generated_text.txt'
-generate_text(path, 100)
+generate_text(path, 20)
 text = copy_text(path)
 print(text)
+
 data = zipp(text)
-print(data)
-print(len(text), len(data))
 write_text(path, data)
+print(data)
+data = unzipp(data)
+print(data)
+
+data1 = zipp_groupby(text)
+write_text(path, data1)
+print(data1)
+data1 = unzipp(data1)
+print(data1)
